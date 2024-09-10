96deacaa1d9fe94ebf922c3c05257a2c48077e72
Alec Liu <sammiliu71@gmail.com>
2024-09-09 17:56:23 -0700

    first commit

---
+++ src/calculators/timeCalc.java
@@ -0,0 +1,39 @@
+package calculators;
+
+import java.util.Scanner;
+
+public class timeCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Start time? Please enter in 24h format:");
+		
+		int start = input.nextInt();
+		
+		System.out.println("Time intervals:");
+		
+		int interval = input.nextInt();
+		
+		System.out.println("End time? Please enter in 24h format:");
+		
+		int end = input.nextInt();
+		
+		for(int i = start; i < end; i+=25) {
+			int min = i % 100;
+//			System.out.println(min);
+			if(min >= 60) {
+				i=(i-min)+100+(min-60);
+				System.out.println(i);
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+		
+		System.out.println(1234 % 100);
+
+	}
+
+}
